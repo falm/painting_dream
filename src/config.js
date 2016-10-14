1735fd1ee042a769671e9aaea5c2527da94d866c
Jason Hou <hjj1992@gmail.com>
2016-10-14 10:09:46 +0800

    add config file

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+const Config = {
+
+    seconds: 8000,
+    // testFile: 'test_data_large.json'
+    testFile: 'test_data_min.json'
+    // testFile: 'test_data.json'
+
+};
+
+export default Config;
